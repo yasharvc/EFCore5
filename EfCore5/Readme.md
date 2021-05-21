e0c7f7dcdc63edb3e7b7354a92e38d1e4a6c3ca8
Yashar Aliabbasi <yashar.aliabbasi@resinasoft.com>
2021-05-21 23:07:57 +0300

    Added some basic Data annotations and created Readme.Md

---
+++ EfCore5/Readme.md
@@ -0,0 +1,2 @@
+# For full list of Data Annotation
+https://docs.microsoft.com/en-us/aspnet/core/mvc/models/validation?view=aspnetcore-5.0
---
+++ EfCore5/src/infrastructure/Shared/Entities/Chapter01/Person.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Entities.Chapter01
+{
+	public class Person : Entity
+	{
+		[Required]
+		[MaxLength(50)]
+		public string FirstName { get; set; }
+		[Required]
+		[MaxLength(50)]
+		public string LastName { get; set; }
+	}
+}
